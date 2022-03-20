2136d2c79453802b3c49563a9da15c1d97f4a361
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-03-20 16:09:10 +0600

    [+]Added unknown argument exception

---
+++ src/main/java/it/me/backyou/controller/exception/UnknownArgumentException.java
@@ -0,0 +1,10 @@
+package it.me.backyou.controller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class UnknownArgumentException extends ResponseStatusException {
+    public UnknownArgumentException() {
+        super(HttpStatus.BAD_REQUEST, "Unknown argument passed");
+    }
+}

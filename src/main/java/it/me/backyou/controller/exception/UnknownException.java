635f1d270748eb2c2ec98d6ac19116cb1e125fa0
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-03-20 12:38:08 +0600

    [ ]Corrected unknown exception status code

--- src/main/java/it/me/backyou/controller/exception/UnknownException.java
+++ src/main/java/it/me/backyou/controller/exception/UnknownException.java
@@ -1,10 +1,11 @@
 package it.me.backyou.controller.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.server.ResponseStatusException;
 
 public class UnknownException extends ResponseStatusException {
     public UnknownException() {
-        super(HttpStatus.valueOf(520), "Unknown error");
+        super(HttpStatus.valueOf(500), "Unknown error");
     }
 }

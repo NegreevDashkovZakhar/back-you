83b10632264f620d1f28715f83f5aa5ada40d1cf
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-03-20 07:36:44 +0600

    [+]Initialized spring app

---
+++ src/main/java/it/me/backyou/BackYouApplication.java
@@ -0,0 +1,13 @@
+package it.me.backyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackYouApplication.class, args);
+	}
+
+}

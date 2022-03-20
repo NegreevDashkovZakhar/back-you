dcc325cecdb1b488cb98d53c50371483110849f5
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-03-20 11:42:33 +0600

    [+]Added name is already in use exception

---
+++ src/main/java/it/me/backyou/controller/exception/UsedNameException.java
@@ -0,0 +1,10 @@
+package it.me.backyou.controller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class UsedNameException extends ResponseStatusException {
+    public UsedNameException() {
+        super(HttpStatus.NOT_ACCEPTABLE, "Name is already in use");
+    }
+}

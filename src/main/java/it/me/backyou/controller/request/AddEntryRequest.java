fafce2a1d03ced35fb9b6c0cc85316160842f92d
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-03-21 16:55:09 +0600

    [+]Added add entry request body class

---
+++ src/main/java/it/me/backyou/controller/request/AddEntryRequest.java
@@ -0,0 +1,35 @@
+package it.me.backyou.controller.request;
+
+public class AddEntryRequest {
+    private String[] columns;
+    private String[] values;
+
+    public AddEntryRequest(final String[] columns, final String[] values) {
+        this.columns = columns;
+        this.values = values;
+    }
+
+    public String[] getColumns() {
+        return columns;
+    }
+
+    public void setColumns(final String[] columns) {
+        this.columns = columns;
+    }
+
+    public String[] getValues() {
+        return values;
+    }
+
+    public void setValues(final String[] values) {
+        this.values = values;
+    }
+}
+/*
+
+{
+    values: [],
+    entries: []
+}
+
+* */

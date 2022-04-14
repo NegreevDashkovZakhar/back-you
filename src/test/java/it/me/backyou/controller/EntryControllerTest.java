0c4fc02ec13762f0c1c7bb215dc29a80d274edcb
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-04-14 13:02:54 +0600

    [+]Added entry and table controller tests

---
+++ src/test/java/it/me/backyou/controller/EntryControllerTest.java
@@ -0,0 +1,53 @@
+package it.me.backyou.controller;
+
+import it.me.backyou.controller.request.AddEntryRequest;
+import it.me.backyou.service.entry.IEntryService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EntryControllerTest {
+    private static EntryController entryController;
+    private static IEntryService entryService;
+
+    @BeforeAll
+    public static void setUp() {
+        entryService = Mockito.mock(IEntryService.class);
+        entryController = new EntryController(entryService);
+    }
+
+    @Test
+    public void testGetAllEntries() {
+        String apiKey = "testapikey";
+        String tableName = "testtablename";
+        Object expected = new Object();
+        Mockito.when(entryService.getAllEntries(apiKey, tableName)).thenReturn(expected);
+        Object actual = entryController.getAllEntries(apiKey, tableName);
+        Mockito.verify(entryService, Mockito.times(1)).getAllEntries(apiKey, tableName);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddEntry() {
+        String apiKey = "testapikey";
+        String tableName = "testtablename";
+        String[] columns = new String[]{"1", "2"};
+        String[] values = new String[]{"qwe", "rty"};
+        AddEntryRequest addEntryRequest = new AddEntryRequest(columns, values);
+
+        entryController.addEntry(apiKey, tableName, addEntryRequest);
+        Mockito.verify(entryService, Mockito.times(1)).addEntry(apiKey, tableName, columns, values);
+    }
+
+    @Test
+    public void testDeleteEntries() {
+        String apiKey = "testapikey";
+        String tableName = "testtablename";
+        String columnName = "column";
+        String value = "val";
+        entryController.deleteEntries(apiKey, tableName, columnName, value);
+        Mockito.verify(entryService, Mockito.times(1)).deleteEntries(apiKey, tableName, columnName, value);
+    }
+}
---
+++ src/test/java/it/me/backyou/controller/TableControllerTest.java
@@ -0,0 +1,55 @@
+package it.me.backyou.controller;
+
+import it.me.backyou.service.table.ITableService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TableControllerTest {
+    private static TableController tableController;
+    private static ITableService tableService;
+
+    @BeforeAll
+    public static void setUp() {
+        tableService = Mockito.mock(ITableService.class);
+        tableController = new TableController(tableService);
+    }
+
+    @Test
+    public void testCreateTable() {
+        String apiKey = "testapikey";
+        String tableName = "testtablename";
+        tableController.createTable(apiKey, tableName);
+        Mockito.verify(tableService, Mockito.times(1)).createTable(apiKey, tableName);
+    }
+
+    @Test
+    public void testDropTable() {
+        String apiKey = "testapikey";
+        String tableName = "testtablename";
+        tableController.dropTable(apiKey, tableName);
+        Mockito.verify(tableService, Mockito.times(1)).dropTable(apiKey, tableName);
+    }
+
+    @Test
+    public void testRenameable() {
+        String apiKey = "testapikey";
+        String tableName = "testtablename";
+        String newTableName = "newtablename";
+        tableController.renameTable(apiKey, tableName, newTableName);
+        Mockito.verify(tableService, Mockito.times(1)).renameTable(apiKey, tableName, newTableName);
+    }
+
+    @Test
+    public void testGetHeaderData() {
+        String apiKey = "testapikey";
+        String tableName = "testtablename";
+        Object expected = new Object();
+        Mockito.when(tableService.getTableHeaderData(apiKey, tableName)).thenReturn(expected);
+        Object actual = tableController.getTableHeaderData(apiKey, tableName);
+        Mockito.verify(tableService, Mockito.times(1)).getTableHeaderData(apiKey, tableName);
+        assertEquals(expected, actual);
+    }
+}

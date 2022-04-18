b0f51fc5686473830223b8b42a9a2e9d3bb5a129
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-04-18 22:56:19 +0600

    [-]Removed unused repository class

--- src/main/java/it/me/backyou/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package it.me.backyou.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Repository for working with users table
- */
-public interface UserRepository extends JpaRepository<User, Long> {
-}

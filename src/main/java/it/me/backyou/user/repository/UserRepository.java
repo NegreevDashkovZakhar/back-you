a462ed6a1f9be78ff94a10aa6db0e24a54ca55c5
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-04-18 22:57:31 +0600

    [+]Added user repository

---
+++ src/main/java/it/me/backyou/user/repository/UserRepository.java
@@ -0,0 +1,31 @@
+package it.me.backyou.user.repository;
+
+import it.me.backyou.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * Repository for working with users table
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    /**
+     * Method checking whether user with such email exists
+     *
+     * @param email email of the user to be checked
+     * @return true if user with such email is registered, false otherwise
+     */
+    @Query(nativeQuery = true,
+            value = "SELECT 1 FROM users WHERE email = ?1 ;")
+    boolean userExist(String email);
+
+    /**
+     * Method for getting users id from email and password
+     *
+     * @param email    email of the user
+     * @param password password of the user
+     * @return users id if found user with such email and password, null otherwise
+     */
+    @Query(nativeQuery = true,
+            value = "SELECT user_id FROM users WHERE email = ?1 AND password = ?2 ;")
+    Long getUserId(String email, String password);
+}

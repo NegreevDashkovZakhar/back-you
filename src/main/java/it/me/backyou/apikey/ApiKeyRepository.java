fe69712f2a991d51c6497c46acd87659ce93bdba
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-04-26 16:08:12 +0600

    [+]Added has api key method to api key repository

--- src/main/java/it/me/backyou/apikey/ApiKeyRepository.java
+++ src/main/java/it/me/backyou/apikey/ApiKeyRepository.java
@@ -15,6 +15,16 @@ public interface ApiKeyRepository extends JpaRepository<ApiKey, Long> {
      * @return api key instance with specified value
      */
     @Query(nativeQuery = true,
-            value = "SELECT * FROM api_key WHERE apiKey = ?1 ;")
+            value = "SELECT * FROM api_key WHERE api_key.api_key = ?1 ;")
     ApiKey getApiKeyByValue(String apiKeyValue);
+
+    /**
+     * Method checking whether table holds given api key
+     *
+     * @param apiKey specified api key
+     * @return 1 if table has entry with specified api key, 0 otherwise
+     */
+    @Query(nativeQuery = true,
+            value = "SELECT COUNT(1) FROM api_key WHERE api_key.api_key = ?1 ;")
+    int hasApiKey(String apiKey);
 }

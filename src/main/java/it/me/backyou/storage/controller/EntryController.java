2558c1be3f1f7ad0246150d097edae81d615dc1b
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-05-05 12:47:18 +0600

    [+]Added cross-origin annotation for storage controllers

--- src/main/java/it/me/backyou/storage/controller/EntryController.java
+++ src/main/java/it/me/backyou/storage/controller/EntryController.java
@@ -5,6 +5,7 @@ import it.me.backyou.storage.service.entry.IEntryService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Controller class.
  * Mapped to respond to requests with entries (insertion, getting etc.)
  */
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(path = "entry")
 public class EntryController {
